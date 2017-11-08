d63a7c43f87e46e39eac901da56b568b6e91f9a3
Andy Fry <andyfry01@gmail.com>
2017-11-08 12:19:04 -0500

    JS cleanup, notes update

--- js/app.js
+++ js/app.js
@@ -35,107 +35,111 @@ $(document).ready(function(){
     // Start router
     router.resolve();
 
-  })();
+    function loadSubCategory(targetDiv, pageRoute) {
+      if ($('.homePage').length < 1) {
+        loadPage('#target', '../pages/home/home.html', () => {
+          loadCategoryPane('#subcategoryTarget', pageRoute)
+          return true
+        })
+      }
+      loadCategoryPane('#subcategoryTarget', pageRoute)
+      return true
+    }
 
-  function loadSubCategory(targetDiv, pageRoute) {
-    if ($('.homePage').length < 1) {
-      loadPage('#target', '../pages/home/home.html', () => {
-        loadCategoryPane('#subcategoryTarget', pageRoute)
-        return true
+    function loadCategoryPane(targetDiv, pageRoute){
+      $(targetDiv).load(pageRoute, () => {
+        $('.close').click(() => {
+          router.navigate('/')
+        })
       })
     }
-    loadCategoryPane('#subcategoryTarget', pageRoute)
-    return true
-  }
 
-  function loadCategoryPane(targetDiv, pageRoute){
-    $(targetDiv).load(pageRoute)
-  }
-
-  function loadPage(targetDiv, pageRoute, next) {
-    Promise.all([
-      getDimensions(window, 'first'),
-      getDimensions('.pageHeader', 'second')
-    ]).then(dimensions => {
-      let dimensionsObj = transformDimensionsArrayIntoObject(dimensions)
-      getTargetDivDimensions(dimensionsObj)
-      .then(dimensions => {
-        setTargetDivHeight(targetDiv, dimensions.height)
-        .then(() => {
-          $(targetDiv).empty()
-          $(targetDiv).load(pageRoute, () => {
-            console.log('pageRoute is', pageRoute);
-            if (pageRoute === '../pages/home/home.html') {
-              $('#currentPage').addClass('height100')
-            }
-            if (next) {
-              next()
-            }
+    function loadPage(targetDiv, pageRoute, next) {
+      Promise.all([
+        getDimensions(window, 'first'),
+        getDimensions('.pageHeader', 'second')
+      ]).then(dimensions => {
+        let dimensionsObj = transformDimensionsArrayIntoObject(dimensions)
+        getTargetDivDimensions(dimensionsObj)
+        .then(dimensions => {
+          setTargetDivHeight(targetDiv, dimensions.height)
+          .then(() => {
+            $(targetDiv).empty()
+            $(targetDiv).load(pageRoute, () => {
+              console.log('pageRoute is', pageRoute);
+              if (pageRoute === '../pages/home/home.html') {
+                $('#currentPage').addClass('height100')
+              }
+              if (next) {
+                next()
+              }
+            })
           })
         })
       })
-    })
-  }
+    }
 
-  function transformDimensionsArrayIntoObject(dimensions){
-    return dimensions.reduce(function(accumulator, current) {
-      accumulator[current.position] = current;
-      return accumulator;
-    }, {});
-  }
+    function transformDimensionsArrayIntoObject(dimensions){
+      return dimensions.reduce(function(accumulator, current) {
+        accumulator[current.position] = current;
+        return accumulator;
+      }, {});
+    }
 
-  function getDimensions(target, positionInEquation) {
-    return new Promise((resolve, reject) => {
-      let targetName = undefined
-      if (target.console) {
-        targetName = 'window'
-      } else {
-        targetName = target
-      }
-      resolve({
-        name: targetName,
-        position: positionInEquation,
-        height: $(target).height(),
-        width: $(target).width()
+    function getDimensions(target, positionInEquation) {
+      return new Promise((resolve, reject) => {
+        let targetName = undefined
+        if (target.console) {
+          targetName = 'window'
+        } else {
+          targetName = target
+        }
+        resolve({
+          name: targetName,
+          position: positionInEquation,
+          height: $(target).height(),
+          width: $(target).width()
+        })
       })
-    })
-  }
+    }
 
-  function getTargetDivDimensions(dimensionsObject){
-    return new Promise((resolve, reject) => {
-      let height = dimensionsObject['first'].height - dimensionsObject['second'].height
-      resolve({height: height})
-    })
-  }
+    function getTargetDivDimensions(dimensionsObject){
+      return new Promise((resolve, reject) => {
+        let height = dimensionsObject['first'].height - dimensionsObject['second'].height
+        resolve({height: height})
+      })
+    }
 
-  function setTargetDivHeight(targetDiv, height){
-    return new Promise((resolve, reject) => {
-      $(targetDiv).height(height)
-      resolve()
-    })
-  }
+    function setTargetDivHeight(targetDiv, height){
+      return new Promise((resolve, reject) => {
+        $(targetDiv).height(height)
+        resolve()
+      })
+    }
 
-  function sequentiallyToggle(current, next){
-    const numImages = 4
-    let currentImageIndex = undefined
-    let nextImageIndex = undefined
-    let currentPic = '.mover-' + current
-    let nextPic = '.mover-' + next
-    $(currentPic).toggleClass('onTop')
-    $(currentPic).toggleClass('hidden')
-    $(nextPic).toggleClass('onTop')
-    $(nextPic).toggleClass('hidden')
-    imgTimer = window.setTimeout(function() {
-      console.log('timeout fired?');
-      if (next < numImages) {
-        currentImageIndex = next
-        nextImageIndex = next + 1
-      } else {
-        currentImageIndex = numImages
-        nextImageIndex = 1
-      }
-      sequentiallyToggle(currentImageIndex, nextImageIndex)
-    }, 7000)
-  }
+    function sequentiallyToggle(current, next){
+      const numImages = 4
+      let currentImageIndex = undefined
+      let nextImageIndex = undefined
+      let currentPic = '.mover-' + current
+      let nextPic = '.mover-' + next
+      $(currentPic).toggleClass('onTop')
+      $(currentPic).toggleClass('hidden')
+      $(nextPic).toggleClass('onTop')
+      $(nextPic).toggleClass('hidden')
+      imgTimer = window.setTimeout(function() {
+        console.log('timeout fired?');
+        if (next < numImages) {
+          currentImageIndex = next
+          nextImageIndex = next + 1
+        } else {
+          currentImageIndex = numImages
+          nextImageIndex = 1
+        }
+        sequentiallyToggle(currentImageIndex, nextImageIndex)
+      }, 7000)
+    }
+  })();
+
 
 })
--- notes.md
+++ notes.md
@@ -1,14 +1,41 @@
 # Todo list:
 
 ## Planning
-1. Check table that steve sent, go over different pages that are required
-2. Ask Steve which pages from the table represent which existing pages so we can copy that content over
-3.
+1. Check table that steve sent, go over different pages that are required - DONE
+2. Ask Steve which pages from the table represent which existing pages so we can copy that content over - DONE
 
 ## Development
-- Who/What/Who should open up a new div with subsections (see table)
-- Add requisite buttons to these new divs which take you to the right pages
+- Who/What/Who should open up a new div with subsections (see table) - DONE
+- Add requisite buttons to these new divs which take you to the right pages  - DONE
 - Add who/what/who to the menu bar.... somehow
-- configure router for the who/what/who endpoints
-- set up folder directory for who/what/who pages
-- add Babel for transpiling 
+- configure router for the who/what/who endpoints - DONE
+- set up folder directory for who/what/who pages - DONE
+- add Babel for transpiling
+
+## Charts
+Use this: [google charts](https://developers.google.com/chart/interactive/docs/quick_start)
+
+
+## Notes from call: 
+
+consultants: exiting content
+structure: exiting content (first page)
+principles: existing content
+we support: want a graphic to come up like in the matrix document with links, some external some internal
+
+
+services: existing, steve will send
+
+seminars: existing?
+
+comprehensive reviews: pie charts from steve
+
+limited reviews: still to be created
+
+principles and credentials  (who we are) (both front and principles page)
+
+past and current: existing (two columns, keep it that way)
+
+research and resources: insurance choices still to be developed, otherwise existing content is being used
+
+surveys: all external links
