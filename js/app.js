5be1d69ad51355388fc7bfe217019bbf12c73b72
Andy Fry <andyfry01@gmail.com>
2017-11-10 13:07:08 -0500

    something something env variable

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-  const pageDomain = 'https://andyfry01.github.io/HBC-SLBA/'
-  // const pageDomain = 'http://127.0.0.1:5000/'
-  // const pageDomain = '../'
+  // const pageDomain = 'https://andyfry01.github.io/HBC-SLBA/'
+  const pageDomain = '../'
 
   let imgTimer = undefined;
 
