7f43d8bbf88cddd5fbefb7e06d01959a3d38ab00
Andy <andyfry01@gmail.com>
2021-11-24 15:42:11 -0700

    Latest steve changes

---
+++ preview-images/chartConfig.js
@@ -0,0 +1,35 @@
+var chartLabels = [
+  "External and Internal Environment",
+  "Mission",
+  "Eligibility and Enrollment System",
+  "Benefits",
+  "Legal Compliance",
+  "Funding System",
+  "Vendor Selection",
+  "Marketing",
+  "Ongoing Management and Customer Service"
+]
+
+var chartColors = [
+  "red",
+  "#FFA500",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet",
+  "turquoise",
+  "brown"
+]
+
+var chartSectionSizes = [
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1,
+  1
+]
